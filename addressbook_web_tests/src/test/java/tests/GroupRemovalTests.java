321d32e07c1ba62c2db59398f304f9929a96051e
dmitr <Dmitry-perfilev94@yande.ru>
2024-01-02 13:02:26 +0300

    Выделены классы помощники

--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -7,11 +7,10 @@ public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
-        app.openGroupsPage();
-        if (!app.isGroupPresent()) {
-            app.createGroup(new GroupData("group header", "group name", "group footer"));
+        if (!app.groups().isGroupPresent()) {
+            app.groups().createGroup(new GroupData("group header", "group name", "group footer"));
         }
-        app.removeGroup();
+        app.groups().removeGroup();
 
     }
 
