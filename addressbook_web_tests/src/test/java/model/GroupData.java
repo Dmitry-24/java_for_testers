a6d2d3f3b6b9c4e129f6165797bb1a03677c5f03
dmitr <Dmitry-perfilev94@yande.ru>
2024-01-05 15:40:04 +0300

    Реализована двухуровневая архитектура тестового набора

--- addressbook_web_tests/src/test/java/model/GroupData.java
+++ addressbook_web_tests/src/test/java/model/GroupData.java
@@ -1,6 +1,6 @@
 package model;
 
-public record GroupData(String header, String name, String footer) {
+public record GroupData(String name, String header, String footer) {
 
     public GroupData() {
         this("", "", "");
