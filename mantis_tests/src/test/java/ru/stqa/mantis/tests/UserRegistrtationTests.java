3a0c97e6cbb8365279e790c0ce408482dd7b5a53
dmitr <Dmitry-perfilev94@yande.ru>
2024-02-11 16:10:43 +0300

    Выполнено задание 18

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrtationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrtationTests.java
@@ -14,12 +14,13 @@ public class UserRegistrtationTests extends TestBase {
     void canRegisterUser() {
         var username = CommonFunctions.randomString(8);
         var password = "password";
-        var email = String.format("%slocalhost", username);
+        var email = String.format("%s@localhost", username);
         app.jamesCli().addUser(email, password);
         app.user().userRegistrationForMainForm(username, email);
         var messages = app.mail().receive(email, password, Duration.ofSeconds(60));
         var url = app.mail().getUrl(messages);
-        app.user().confirmData(url, "test", password);
+        app.user().confirmData(url, "testName", password);
+        app.http().login(username, password);
         Assertions.assertTrue(app.http().isLoggedIn());
 
     }
