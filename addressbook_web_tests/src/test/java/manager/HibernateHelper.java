28902b5b4ede5d147b1f2284ab7cfb5937d4b04f
dmitr <Dmitry-perfilev94@yande.ru>
2024-02-17 12:39:16 +0300

    Выполнено задание 20

--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -1,5 +1,6 @@
 package manager;
 
+import io.qameta.allure.Step;
 import manager.hbm.ContactRecord;
 import manager.hbm.GroupRecord;
 import model.ContactData;
@@ -76,7 +77,7 @@ public class HibernateHelper extends HelperBase {
         return new ContactRecord(Integer.parseInt(id), data.firstName(), data.lastName(), data.address(), data.phone(), data.email());
     }
 
-
+    @Step
     public List<GroupData> getGroupList() {
         return convertGroupList(sessionFactory.fromSession(session -> {
             return session.createQuery("from GroupRecord", GroupRecord.class).list();
@@ -104,6 +105,7 @@ public class HibernateHelper extends HelperBase {
         }));
     }
 
+    @Step
     public void createGroup(GroupData groupData) {
         sessionFactory.inSession(session -> {
             session.getTransaction().begin();
